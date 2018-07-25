4bb2e986c8f61a63d0541499ab5a960dfc7dc2da
Samuel Nayerman <snayerman@godaddy.com>
2018-07-25 02:10:47 -0700

    baseline component done

---
+++ src/Components/ModalScreen/AssistModal.jsx
@@ -0,0 +1,131 @@
+import React, { Component } from 'react';
+import { ButtonToolbar, Button, FormGroup, InputGroup, FormControl, Glyphicon } from 'react-bootstrap';
+import Axios from 'axios';
+
+import './ModalScreen.scss';
+const wellStyles = { maxWidth: 450, margin: '0 auto ', color:'white'};
+
+
+export default class AssistModal extends Component {
+   state = {
+      firstQuestion:true,
+      second:false,
+      third:false
+   };
+   
+   renderFirst() {
+      return (
+         <div style={{backgroundColor:'#49a34c', textAlign:'center'}}>
+         <div className="innerModal">
+         <img
+               src="./assets/GD_HEAD_RGB_FC.svg"
+               type="image/svg+xml"
+               style={{height:'15vh'}}
+            >
+            </img>
+            <h4 style={{marginRight:'20px', marginLeft:'20px', marginBottom:'20px', color:'white'}}>What goals do you have with GoDaddy?</h4>
+            <FormGroup controlId="formControlsTextarea" style={{marginRight:'40px', marginLeft:'40px', marginBottom:'20px'}}>
+               <InputGroup id="queryInput">
+                  <FormControl componentClass="textarea" style={{height:'10vh'}} type="text" placeholder="Try: I need to start an online presence for my business" onSubmit={() => console.log("SUBMIT")}/>
+                  <InputGroup.Addon className="glyphbtn">
+                     <Button bsStyle="primary" bsSize="xsmall" style={{color:'black', backgroundColor:'white'}}>
+                        <Glyphicon glyph="chevron-right" onClick={() => this.setState({firstQuestion:false, second:true, third:false})}></Glyphicon>
+                     </Button>
+                  </InputGroup.Addon>
+               </InputGroup>
+            </FormGroup>
+         </div>
+         </div>
+      );
+   }
+   
+   renderSecond() {
+      return (
+         <div style={{backgroundColor:'#49a34c', textAlign:'center'}}>
+         <div className="innerModal">
+         <img
+               src="./assets/GD_HEAD_RGB_FC.svg"
+               type="image/svg+xml"
+               style={{height:'15vh'}}
+            >
+            </img>
+            <h4 style={{marginRight:'20px', marginLeft:'20px', marginBottom:'20px', color:'white'}}>Great! We're glad to help you create your online presence.
+            </h4>
+            <h5 style={{color:'white'}}>To help us find the right products for you, could you tell us if you have any technical experience?
+            </h5>
+            <div style={wellStyles}>
+               <ButtonToolbar style={{marginTop:'10px',height:'15vh', align:'center', color:'white'}}>
+               <Button style={{height:'8vh', color:'white', backgroundColor:'#d02f85'}}bsStyle="primary">
+               I'm a developer <br/> pro!
+               </Button>
+               <Button style={{height:'8vh', color:'white', backgroundColor:'#d02f85'}} bsStyle="primary">
+               I've dabbled <br/>with HTML.
+               </Button>
+               <Button onClick={()=>this.setState({firstQuestion:false,second:false,third:true})}style={{height:'8vh', color:'white', backgroundColor:'#d02f85'}} bsStyle="primary">
+               I'm new!
+               </Button>
+               </ButtonToolbar>
+            </div>
+         </div>
+         </div>
+      );
+   }
+
+   renderThird() {
+      return (
+         <div style={{backgroundColor:'#49a34c', textAlign:'center'}}>
+         <div className="innerModal">
+         <img
+               src="./assets/GD_HEAD_RGB_FC.svg"
+               type="image/svg+xml"
+               style={{height:'15vh'}}
+            >
+            </img>
+            <h4 style={{marginRight:'20px', marginLeft:'20px', marginBottom:'20px', color:'white'}}>Don't fret! We love helping those without technical experience.
+            </h4>
+            <h5 style={{color:'white'}}>Would you consider having a custom web designer build your site for you?
+            </h5>
+            <FormGroup controlId="formControlsTextarea" style={{marginRight:'40px', marginLeft:'40px', marginBottom:'20px'}}>
+               <InputGroup id="queryInput">
+                  <FormControl componentClass="textarea" type="text" placeholder="" onSubmit={() => console.log("SUBMIT")}/>
+                  <InputGroup.Addon className="glyphbtn">
+                     <Button bsStyle="primary" bsSize="xsmall" style={{color:'black', backgroundColor:'white'}}>
+                        <Glyphicon glyph="chevron-right" onClick={() => this.setState({firstQuestion:false, second:false, third:false})}></Glyphicon>
+                     </Button>
+                  </InputGroup.Addon>
+               </InputGroup>
+            </FormGroup>
+         </div>
+         </div>
+      );
+   }
+
+   render() {
+      if(this.state.firstQuestion) {
+         return(
+            <div>
+               {this.renderFirst()}
+            </div>
+         )
+      } else if(this.state.second) {
+         return(
+            <div>
+               {this.renderSecond()}
+            </div>
+         )
+      } else if(this.state.third) {
+         return(
+            <div>
+               {this.renderThird()}
+            </div>
+         )
+      } else {
+         return(
+            <div>THIS IS YENGS COMPONENT HERE</div>
+         )
+      }
+
+   }
+
+  
+}
